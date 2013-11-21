3e4fd0c1e1b6143097b18b397ce5873b0d57c16d
ekoon <aerospike1111@gmail.com>
2013-11-21 02:06:05 -0500

    mating pool update

--- bitstring/MakingMatingPool.m
+++ bitstring/MakingMatingPool.m
@@ -1,4 +1,4 @@
 % Needs to return an even number.
-function[MatingPool] = MakingMatingPool(Pop, sigma)
+function[PopReference] = MakingMatingPool(Pop, sigma)
     PopReference = ceil(abs((randn(size(Pop,1),1) * sqrt(sigma) )));
 end
